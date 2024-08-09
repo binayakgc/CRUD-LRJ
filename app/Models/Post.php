98ce26ff18f14532fee10232ed2f0f0bedd3de27
binayakgc <binayakgc@gmail.com>
2024-08-09 16:13:55 +1000

    Login and register

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Model as EloquentModel;
 
-class Post extends Model
+class Post extends EloquentModel
 {
     use HasFactory;
     protected $fillable=['title','content'];
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::get('/', function() {
-    return redirect('/posts');
+    return redirect('/login');
 });
 
 Route::resource('posts', PostController::class); 
 Route::delete('posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
